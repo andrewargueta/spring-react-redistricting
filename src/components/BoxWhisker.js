aac593e590b87fb6cb5d831e80a9d11fb2760c6f
Andrew Argueta <andrew.argueta@stonybrook.edu>
2020-12-15 21:07:41 -0500

    plot data improveed

--- src/components/BoxWhisker.js
+++ src/components/BoxWhisker.js
@@ -24,13 +24,13 @@ class BocWhisker extends React.Component {
         super(props);
         this.state = {
             plotData:{},
-            // trace1: {
-            //     y: [0, 0, 0, 0],
-            //     x: ['1', '1', '1', '1'],
-            //     name: 'Orginal',
-            //     marker: {color: '#3D9970'},
-            //     type: 'box'
-            // },
+            trace1: {
+                y: [0, 0, 0, 0],
+                x: ['1', '1', '1', '1'],
+                name: 'Orginal',
+                marker: {color: '#3D9970'},
+                type: 'box'
+            },
             trace2: this.props.trace2?this.props.trace2:{
                 y: this.props.plotData[0]?[this.props.plotData[0].min,this.props.plotData[0].q1,
                     this.props.plotData[0].median,this.props.plotData[0].q3, this.props.plotData[0].max]: [0,0,0,0,0] ,/*min  ?? ?? max*/
@@ -46,18 +46,19 @@ class BocWhisker extends React.Component {
 
     componentDidUpdate(prevProps, prevState) {
         if(prevProps.plotData !== this.props.plotData){
-            this.setState({trace2: this.props.plotData});
+            this.setState({trace2: this.props.plotData[1]});
+            this.setState({trace1: this.props.plotData[0]});
         }    
       }
 
 
     render() {
-        // var trace1= this.state.trace1;
+        var trace1= this.state.trace1;
         var trace2 = this.state.trace2;
         return (
                 <Plot
                     data={[
-                        // trace1,
+                        trace1,
                         trace2,
                     ]}
                     layout={config.layout}
